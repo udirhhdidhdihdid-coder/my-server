6b65d4e8dcd1db3dc1e50032b4aaed503cfbe6e7
udirhhdidhdihdid-coder <udirhhdidhdihdid@gmail.com>
2026-08-08 21:45:39 +0300

    Create server.py

---
+++ server.py
@@ -0,0 +1,17 @@
+import os
+from flask import Flask, jsonify
+import random
+
+app = Flask(__name__)
+
+@app.route('/generate-code', methods=['POST'])
+def generate_code():
+    code = str(random.randint(100000, 999999))
+    return jsonify({
+        "success": True,
+        "code": code
+    })
+
+if __name__ == '__main__':
+    port = int(os.environ.get('PORT', 5000))
+    app.run(host='0.0.0.0', port=port)
